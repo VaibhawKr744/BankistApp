ce0d3c103f2f24499333bd79a6b24bb5456f847a
VaibhawKr744 <honeykr744@gmail.com>
2021-01-08 16:14:31 +0530

    test commit2

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
-  pin: 11111,
+  pin: 1111,
 };
-dasdasfabf kajhfkajhfash fak
+
 const account2 = {
   owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
