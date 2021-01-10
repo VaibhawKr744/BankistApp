bcf9b4a64c0e2a82649919af7134d892a929fe5e
VaibhawKr744 <honeykr744@gmail.com>
2021-01-10 14:34:47 +0530

    computing username

--- script.js
+++ script.js
@@ -76,6 +76,18 @@ const displayMovements = function(movements){
   });
 };
 displayMovements(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
