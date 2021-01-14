d06b162e0d537fa0267139956bc271a9020f38d0
VaibhawKr744 <honeykr744@gmail.com>
2021-01-14 18:18:23 +0530

    added transfer feature

--- script.js
+++ script.js
@@ -169,6 +169,17 @@ btnTransfer.addEventListener('click', function(e){
   updateUI(currentAccount);
 });
 
+btnLoan.addEventListener('click', function(e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)){
+      currentAccount.movements.push(amount);
+      updateUI(currentAccount)
+    }
+    inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e){
   e.preventDefault();
   
@@ -187,16 +198,3 @@ btnClose.addEventListener('click', function (e){
   inputCloseUsername.value = inputClosePin.value = '';
 
 });
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
-
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
